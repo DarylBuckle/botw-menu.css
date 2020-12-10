e95a76b85f7b5c7ff09388b02ac3bfa09a2c277a
Daryl Buckle <darylbuckle1@gmail.com>
2020-12-10 21:57:29 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# botw-menu.css
+ A style sheet for Breath of the Wild style menus.
---
+++ example/src/App.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import 'bootstrap/dist/css/bootstrap.css'
+
+import 'botw-menu.css'
+
+
+const App = () => {
+
+  return (
+    <div className="botw-background">
+      <div 
+        className={"botw-menu-item"} 
+      >
+        <div className="botw-menu-item-content">
+          Here is some data.
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default App
