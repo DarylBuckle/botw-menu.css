e194e22687f27b6bef1a801f3d95fa46f3164b4c
Daryl Buckle <darylbuckle1@gmail.com>
2020-12-10 23:26:55 +0000

    Updates

---
+++ precache-manifest.677990e76020d11d5ce58e00aebb403d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9729a685a754bdfd606f51ac9bb5fa68",
+    "url": "./index.html"
+  },
+  {
+    "revision": "dc91bd6af054b88f3377",
+    "url": "./static/css/2.6414bb7b.chunk.css"
+  },
+  {
+    "revision": "65ec93d4c8d0b3790083",
+    "url": "./static/css/main.7ce36635.chunk.css"
+  },
+  {
+    "revision": "dc91bd6af054b88f3377",
+    "url": "./static/js/2.84caf9bf.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.84caf9bf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "65ec93d4c8d0b3790083",
+    "url": "./static/js/main.99e9b8d6.chunk.js"
+  },
+  {
+    "revision": "b6d9714bde259d737159",
+    "url": "./static/js/runtime-main.90111754.js"
+  }
+]);
